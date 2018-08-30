b8c4231470ae07110e427eb545d2a14f7552e441
user <user@JBTC10111>
2018-08-30 15:18:02 +0900

    03

--- 자바기초실습/src/제02차시/Class01.java
+++ 자바기초실습/src/제02차시/Class01.java
@@ -4,7 +4,7 @@ class Class01 {
 	int i=0;
 	
 	Class01(){
-		i+=3;
+		i+=3; 
 		System.out.println("난 Class01 클래스의 생성자란다.");
 		System.out.println("Class01 클래스의 모양으로 객체를 만든 즉시 호출됐지.");
 		System.out.println("난i, 필드 또는 맴버 변수라고도 하지. c언어의 전역 변수 정도로 이해하자.");

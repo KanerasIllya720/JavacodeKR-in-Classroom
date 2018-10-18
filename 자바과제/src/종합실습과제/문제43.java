3427598d1aacd071a96ea33e04317e47262d3a7d
user <user@JBTC10111>
2018-10-18 16:26:01 +0900

    09

---
+++ 자바과제/src/종합실습과제/문제43.java
@@ -0,0 +1,30 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제43 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("자연수 n: ");
+		int n=s.nextInt();
+		int sum=0;
+		int nums[]=new int[n];
+		for(int i=0;i<n;i++) {
+			nums[i]=i;
+		}
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n;j++) {
+				if(nums[i]%i==0)
+					sum++;
+			}
+			if(sum!=2)
+				nums[i]=i;
+			else
+				nums[i]=0;
+			sum=0;
+		}
+		for(int i=0;i<n;i++) {
+			System.out.println(nums[i]+" ");
+		}
+	}
+}
